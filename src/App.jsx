7d0c4bf4f185849ef81100b3c3f809845404fb83
Emanuel_Celis <ecelis813@gmail.com>
2026-04-23 07:21:45 -0500

    Refactor:  Organizar las importaciones de componentes en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-// import TestComponents from './components/pages/TestComponents(CELIS)';
+import TestComponents from './components/pages/TestComponents(CELIS)';
+import Home from './components/pages/Home';
+import Buscar from './components/pages/Buscar';
+import Perfil from './components/pages/Perfil';
+import NotFound from './components/pages/NotFound';
 import Login from './components/pages/Login';
-import Biblioteca from './components/pages/Biblioteca';
 import Register from './components/pages/Register';
+import Biblioteca from './components/pages/Biblioteca';
 import './components/styles/global.css';
 
 function App() {
   return (
-  // <TestComponents />;
-  // <Login /> 
-  // <Biblioteca />
-  <Register />
+
+    //<TestComponents />;
+    // <Biblioteca />
+    // <Buscar />
+    <Home />
+    // <Login />
+    // <NotFound />
+    // <Perfil />
+    // <Register />
+
   )
 }
 
